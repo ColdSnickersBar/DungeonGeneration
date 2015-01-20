bc33a8844db9555964694e73b15c31ce4e81ad76
Adam Williams <awilliams@orbotix.com>
2015-01-20 10:34:07 -0700

    Added RandomRoomSpecGenerator but not the implementation

--- RandomRoomSpecGenerator.cs
+++ RandomRoomSpecGenerator.cs
@@ -2,8 +2,17 @@
 
 namespace DungeonGeneration
 {
-	public class RandomRoomSpecGenerator
+	public class RandomRoomSpecGenerator : IRoomSpecGenerator
 	{
+		#region IRoomSpecGenerator implementation
+
+		public RoomSpec getNext ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+
 		public RandomRoomSpecGenerator ()
 		{
 		}
